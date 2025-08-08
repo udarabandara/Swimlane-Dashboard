6a58a77bebe75eda789c48421433ca395e16690a
udarabandara <udarabandara3@gmail.com>
2025-08-08 12:55:24 +0530

    Title content

--- swimlane-dashboard/src/app/page.js
+++ swimlane-dashboard/src/app/page.js
@@ -2,46 +2,45 @@
 
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import TitleContent from "../components/TitleContent";
 import { useState } from "react";
 
 export default function Home() {
-  const [sidebarOpen, setSidebarOpen] = useState(false); 
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-      <div >
+    <div>
       {/* Header */}
-      <div className={`${
-          sidebarOpen ? "hidden" : "hidblockden"
-        }`}>      <Header /></div>
-
+      <div className={`${sidebarOpen ? "hidden" : "hidblockden"}`}>
+        {" "}
+        <Header />
+      </div>
 
       <div className="w-full flex flex-row min-h-screen">
-      {/* Sidebar */}
-      <div
-        className={`${
-          sidebarOpen ? "block" : "hidden"
-        } fixed z-30 inset-y-0 bg-white left-0 w-64 border-r border-gray-300 p-4 md:relative md:block`}
-      >
-        <div className="flex justify-between items-center mb-4 md:hidden">
-          <span className="text-xl font-bold">Menu</span>
-          <button onClick={() => setSidebarOpen(false)}>✕</button>
-        </div>
-        <Nav />
-      </div>
-
-      {/* Main Content Area */}
-      <div className="flex-1 flex flex-col min-h-screen ml-0 md:ml-64">
-        {/* Topbar for small screens */}
-        <div className="md:hidden bg-white shadow p-4 flex justify-between items-center">
-          <button onClick={() => setSidebarOpen(true)}>☰</button>
+        {/* Sidebar */}
+        <div
+          className={`${
+            sidebarOpen ? "block" : "hidden"
+          } fixed z-30 inset-y-0 bg-white left-0 w-64 border-r border-gray-300 p-4 md:relative md:block`}
+        >
+          <div className="flex justify-between items-center mb-4 md:hidden">
+            <button onClick={() => setSidebarOpen(false)}>✕</button>
+          </div>
+          <Nav />
         </div>
 
-        <main className="flex bg-white">
-         
-        </main>
-      </div>
-    </div>
+        {/* Main Content Area */}
+        <div className="w-full">
+          {/* Topbar for small screens */}
+          <div className="md:hidden bg-white shadow p-4 flex justify-between items-center">
+            <button onClick={() => setSidebarOpen(true)}>☰</button>
+          </div>
 
+          <main className="flex bg-white w-full">
+            <TitleContent />
+          </main>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ swimlane-dashboard/src/components/TitleContent.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+function TitleContent() {
+  return (
+    <div className="px-8 w-full py-4 flex-col border-b border-gray-300">
+      <div className="flex flex-row mb-2">
+        <span className="text-xl font-bold">Sport Xi Project</span>
+         <button className="px-2 bg-amber-500 rounded-md ml-5">
+                <span className="text-sm">In progress</span>
+          </button>
+      </div>
+      <span className="text-lg text-gray-400">event production</span>
+
+      <div className="flex flex-row items-center mt-2">
+      <span className="text-lg text-gray-400">assigned</span>
+
+      <div className="flex -space-x-3 ml-5">
+        <img className="dark:invert w-8 h-8 rounded-full border-2 border-white" src="/user.svg" alt="user"  />
+        <img className="dark:invert w-8 h-8 rounded-full border-2 border-white" src="/user.svg" alt="user"  />
+        <img className="dark:invert w-8 h-8 rounded-full border-2 border-white" src="/user.svg" alt="user"  />
+        <div className="w-8 h-8 rounded-full bg-gray-200 text-gray-700 text-sm font-semibold flex items-center justify-center border-2 border-white">
+          +2
+        </div>
+      </div>
+
+       <button className="px-4 py-1 flex flex-row items-center border border-gray-300 rounded-2xl ml-5">
+            <span className="text-sm text-gray-400 mr-2">Manage</span>
+            <svg  xmlns="http://www.w3.org/2000/svg"  width="18"  height="18"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  strokeWidth="2"  strokeLinecap="round"  strokeLinejoin="round"  className="icon icon-tabler icons-tabler-outline text-gray-400 icon-tabler-pencil"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M4 20h4l10.5 -10.5a2.828 2.828 0 1 0 -4 -4l-10.5 10.5v4" /><path d="M13.5 6.5l4 4" /></svg>
+          </button>
+      </div>
+    </div>
+  )
+}
+
+export default TitleContent
